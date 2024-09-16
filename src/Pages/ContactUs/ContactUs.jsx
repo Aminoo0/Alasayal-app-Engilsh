348be571df0174f9e9145082e393cf336177d4c3
AbdAlrhman Mohamed <abdooamino0@gmail.com>
2024-09-16 10:48:40 +0300

    update

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import ourCarsHeader from '../../assets/our-cars-header.jpg'
 import { useLocation, useParams } from 'react-router-dom'
 import { useForm, ValidationError } from '@formspree/react';
+import { motion } from 'framer-motion';
 
 
 export default function ContactUs() {
@@ -19,7 +20,7 @@ export default function ContactUs() {
     <section className={`pb-32 bg-[url(assets/main-bg.jpg)]`}>
       <div className='w-full mb-10 relative'>
         <div className='w-full bg-slate-700 bg-opacity-50 absolute top-0 left-0 right-0 bottom-0'>
-          <h2 className='text-white text-5xl absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-nowrap'>Contact Us</h2>
+          <motion.h2 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='text-white text-5xl font-bold absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-nowrap'>Contact Us</motion.h2>
         </div>
         <img className='h-[400px] object-cover w-full' src={ourCarsHeader} alt="" />
       </div>
